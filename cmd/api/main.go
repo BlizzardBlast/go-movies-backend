6d991fcb4ef92b187d0ba8143c7c2207e6ba5f5b
BlizzardBlast <64456154+BlizzardBlast@users.noreply.github.com>
2023-07-23 09:26:07 +0700

    Installing a database driver and connecting to Postgres

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
@@ -12,6 +13,7 @@ const port = 8080
 type application struct {
 	DSN string
 	Domain string
+	DB *sql.DB
 }
 
 func main() {
@@ -23,11 +25,17 @@ func main() {
 	flag.Parse()
 
 	// Connect to the database
+	conn, err := app.connectToDB()
+	if (err != nil) {
+		log.Fatal(err)
+	}
+	app.DB = conn
+
 	app.Domain = "example.com"
 
 	log.Println("Starting application on port", port)
 	// Start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
