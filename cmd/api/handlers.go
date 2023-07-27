e1cc710005094be7831deb057f43ad445125dd83
BlizzardBlast <64456154+BlizzardBlast@users.noreply.github.com>
2023-07-27 07:54:14 +0700

    Creating a handler to generate a JWT

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -26,4 +27,30 @@ func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, movies)
+}
+
+func (app *application) authenticate(w http.ResponseWriter, r *http.Request) {
+	// Read json payload
+
+	// Validate user against database
+
+	// Check password
+
+	// Create a jwt user
+	u := jwtUser {
+		ID: 1,
+		FirstName: "Admin",
+		LastName: "User",
+	}
+
+	// Generate tokens
+	tokens, err := app.auth.GenerateTokenPair(&u)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	log.Println(tokens.Token)
+
+	w.Write([]byte(tokens.Token))
 }
